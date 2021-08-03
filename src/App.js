fad832b1a7a2f908256c55c3da9ffbd2395d266c
Daniel <danielfrancisco99@outlook.com>
2021-08-03 12:59:21 -0500

    inicio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Styles/styles.scss"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Menu extends React.Component{
+  constructor(props) {
+    super(props);
+    
+  }
+  render(){
+    
+      return(
+        <>
+         <div id="martyscombo"> 
+          <p>Marty's Combo</p>
+         </div>
+         
+        </>
+      )
+  }
 }
 
-export default App;
+class Whowe extends React.Component{
+  constructor(props) {
+    super(props);
+    
+  }
+  render(){
+    
+      return(
+        <>
+         <p>who we are</p>
+         
+        </>
+      )
+  }
+}
+
+
+class Body extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      content:null,
+    }
+}
+  render(){
+    return(
+        <>
+         <ul class = "nav">
+            <button onClick={()=>{
+             this.setState({content:<Menu/>})}}>Menu</button>
+            <button onClick={()=>{
+             this.setState({content:null})}}>Home</button>
+            <button onClick={()=>{
+             this.setState({content:<Whowe/>})}}>Who we are</button>
+         </ul>
+        <div>{this.state.content}</div>
+        </>
+      )
+  }
+}
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    
+  }
+  render(){
+    
+      return(
+        <>
+         <Body/>
+
+        </>
+      )
+  }
+}
+
+export default App
