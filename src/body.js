2cc4ed11bcd4d2a6f18cc121a792e1556922b697
Daniel <danielfrancisco99@outlook.com>
2021-11-22 02:16:25 -0500

    finished

--- src/body.js
+++ src/body.js
@@ -7,8 +7,9 @@ import uuid from 'react-uuid';
 import { data} from "./data";
 
 var li = []
-
+var rli=[]
 var lick = []
+var cou = 1
 
 class Body extends React.Component{
   constructor(props){
@@ -17,7 +18,8 @@ class Body extends React.Component{
       content:<Home/>,
       nuevo:[],
       check:[],
-      co:0
+      co:0,
+      
       }
     
 }
@@ -26,7 +28,7 @@ clean=()=>{
     
   }
 selec=(combo)=>{
-    
+  //displaying items in cart
   var cont = <ul id="buy" key={data[combo].name}>
   <b>Product</b>  
   <b>Quantity</b> 
@@ -67,21 +69,28 @@ selec=(combo)=>{
       mr()
     })
     .then(()=>{
+      //incresing the quantity of items using the button +
       data[combo].num = data[combo].num+1
      
       })
     .then(()=>{
+      /*emptying the array so the quantity of items can updated
+      when the button + is pressed*/
       this.setState({nuevo:[]})
       
       for(var i in this.state.check){
-        
+        /*adding elements to an array to use it to know the 
+        items that were selected before, the items are added from the end to the
+        beginig of the array because the items change position whe added again*/
        lick.push(this.state.check[(this.state.check.length-1)-i])
-       
+       //the documentation in this button "+" also explains the button "-"
       }
+      //emptiying the check array so the items can be updated
       this.setState({check:[]})
     })
     .then(()=>{
       for(var i in lick){
+        //ading items already updated to cart 
         this.selec(lick[i])
       }
     })
@@ -92,18 +101,24 @@ selec=(combo)=>{
   </div>
 </ul>
 if(this.state.check.includes(data[combo].name)===false && this.state.check.includes("")===false) {
+  //array used it to store items
   this.setState({nuevo:[cont].concat(this.state.nuevo)})
+  //array used it to check if an elemnt was already added to the cart
   this.setState({check:[data[combo].name].concat(this.state.check)})
   
 }
+//switching to cart page
 this.setState({content:this.state.nuevo})
     
 }
 
 componentDidMount(){
+  //adding items from the menu to array so can be displayed
   for(var i in data){
+    
     for(var j in data[i]){
-      
+     if(cou%2===1){
+        
       if(j !== "num" && j==="img") {
         li.push(<img src={data[i][j]} alt="combo" key={uuid()}></img>)
       }
@@ -127,8 +142,43 @@ componentDidMount(){
         li.push(<p key={uuid()}>{data[i][j]}</p>,)
       }
       
+    }
+   
+    if(cou%2===0){
+      
+      if(j !== "num" && j==="img") {
+        rli.push(<img src={data[i][j]} alt="combo" key={uuid()}></img>)
       }
-  }
+      if(j !== "num" && j==="price") {
+        rli.push(<p key={uuid()}>${data[i][j]}</p>)
+      }
+
+      if(j==="name"){
+        rli.push(<button id = {data[i].name}  key={uuid()} onClick={(e)=>{
+          new Promise((mf)=>{
+          mf()
+          })
+          .then(()=>{
+            this.selec(e.target.id)
+            
+          })
+          
+        }}>Buy</button>)
+      }
+
+      if(j !== "num" && j!=="img" && j!=="price") {
+        rli.push(<p key={uuid()}>{data[i][j]}</p>,)
+      }
+      
+    }
+    
+    }
+      
+      
+      cou = cou+1
+      
+}
+ 
 }
 
 render(){
@@ -136,10 +186,12 @@ render(){
         <>
          <ul id = "nav" >
             <button onClick={()=>{
-             this.setState({content:<Menu cont={
-              <div id="martys" key={uuid()} >{li}</div>
-              
-          }/>})}}>Menu</button>
+            //display items in menu
+              this.setState({content:<Menu cont={[
+              <div id="comb" key={uuid()} >{li.map(v=>{return v})}</div>,
+              <div id="combr" key={uuid()} >{rli.map(v=>{return v})}</div>
+            ]}/>}) 
+            }}>Menu</button>
 
             <button onClick={()=>{
              this.setState({content:<Home/>})}}>Home</button>
